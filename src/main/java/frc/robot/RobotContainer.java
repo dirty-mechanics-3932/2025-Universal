68baa2e5d595a26b7fb2fadf7dad50f72d66e3ab
Raphael S. <176762451+ThundRbug88@users.noreply.github.com>
2026-06-11 18:23:49 -0400

    ee

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import frc.robot.platforms.Mando;
 import frc.robot.platforms.MiniIsaac;
 import frc.robot.platforms.KeithMini;
 import frc.robot.platforms.MiniMini;
+import frc.robot.platforms.MiniRaph;
 import frc.robot.platforms.ParadeSrxDriveRobots;
 import frc.robot.platforms.RobotRunnable;
 import frc.robot.platforms.Sibling2025;
@@ -145,6 +146,8 @@ public class RobotContainer {
       case MiniIsaac:
         runnableRobot = Optional.of(new MiniIsaac());
         break;
+      case MiniRaph:
+        runnableRobot = Optional.of(new MiniRaph());
     }
     logf("Finished Creating RobotContainer\n");
   }
